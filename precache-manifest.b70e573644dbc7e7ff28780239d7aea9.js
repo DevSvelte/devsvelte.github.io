282c9380d40306e9c15cc67b62989200023dfc6d
SAMEER TARIQ <imsamtar@gmail.com>
2019-04-29 16:29:04 +0500

    ...

---
+++ precache-manifest.b70e573644dbc7e7ff28780239d7aea9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "d26ad9b6b2d9f23f76dc",
+    "url": "/js/chunk-vendors.5101329d.js"
+  },
+  {
+    "revision": "ddfb145f1db953d985d7",
+    "url": "/js/app.537ee430.js"
+  },
+  {
+    "revision": "839152582e6ee3da7804979d12002efa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34063c132f2e0a766495373dabec4075",
+    "url": "/img/profile.jpeg"
+  },
+  {
+    "revision": "f8e4cf10f32485410c7c6b9fe5cd2c10",
+    "url": "/img/header.jpg"
+  },
+  {
+    "revision": "ddfb145f1db953d985d7",
+    "url": "/css/app.bb7479fe.css"
+  }
+];
